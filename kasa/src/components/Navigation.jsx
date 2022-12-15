33b290e57db0bf3409e87fa84a462daa157a1679
Marc <marc.gotze@gmail.com>
2022-12-15 15:23:13 +0100

    Hotfix nav html

--- kasa/src/components/Navigation.jsx
+++ kasa/src/components/Navigation.jsx
@@ -5,15 +5,16 @@ const Navigation = () => {
   return (
     <div className="nav">
       <ul className="nav__ul">
-        <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-          <li className="nav__homeBtn">Accueil</li>
-        </NavLink>
-        <NavLink
-          to="/about"
-          className={(nav) => (nav.isActive ? "nav-active" : "")}
-        >
-          <li className="nav__aboutBtn">A Propos</li>
-        </NavLink>
+        <li className="nav__homeBtn"> 
+          <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+            Accueil
+          </NavLink>
+        </li>
+        <li className="nav__aboutBtn">
+          <NavLink to="/about" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+            A Propos
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
